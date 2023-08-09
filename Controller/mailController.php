87e66a5ba5b830722ed0901fb3b6f4c00c3ca2f0
Allan Mauricio <allan.work19@gmail.com>
2023-08-09 16:38:45 -0300

    Troca do servidor de e-mail para o da unimed

--- Controller/mailController.php
+++ Controller/mailController.php
@@ -9,9 +9,6 @@ include '../Model/env.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require '../vendor/PHPMailer-master/src/Exception.php';
-require '../vendor/PHPMailer-master/src/PHPMailer.php';
-require '../vendor/PHPMailer-master/src/SMTP.php';
 
 require '../vendor/autoload.php';
 use Firebase\JWT\JWT;
@@ -22,11 +19,10 @@ class mailController{
         // CONEXAO EMAIL
         $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
-        $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-        $phpmailer->SMTPAuth = true;
-        $phpmailer->Port = 2525;
-        $phpmailer->Username = '4075d522bd2a32';
-        $phpmailer->Password = 'b014bec44d1bc8';
+        $phpmailer->Host = 'mail.unimedsjc.com.br';
+        $phpmailer->Port = 25;
+        $phpmailer->SMTPAuth = false;
+        $phpmailer->SMTPAutoTLS = false;
         $phpmailer->CharSet = "UTF-8";
 
         // CONEXAO BANCO
@@ -57,7 +53,7 @@ class mailController{
 
         // PRESET
         $mail = new mail();
-        $mail->setSender('ti.forgot-password@gmail.com');
+        $mail->setSender('ti.forgot-password@unimedsjc.coop.br');
         $mail->setReceiver($u->getEmail());
         $mail->setSubject('Pedido de redefinição de senha');
         $mail->setContent('<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Forgot Password</title><body style="margin:0;width:100vw;height:100vh;font:20px Calibri;">');
@@ -65,7 +61,7 @@ class mailController{
         $mail->setContent('<h2>Olá :]</h2>');
         $mail->setContent('<p>Ficamos sabendo que você esqueceu a senha.☹️<br/>Mas não se preocupe, daremos um jeitinho para você.</p>');
         $mail->setContent('<p style="margin-bottom: 40px; font-weight:bold; color:red;">Caso não tenho sido você que pediu a redefinição de senha, pode ignorar esse e-mail. Caso tenha sido, clique no link abaixo.😉</p>');
-        $mail->setContent('<a href="http://localhost/captiveportal/views/resetPassword.php?email='.$EncodeEmail.'" style="text-align:center;font-size:25px;text-decoration:none;background-color:rgb(67,119,67);color:white;padding:20px 26px;border:2px solid rgb(67,119,67);border-radius:40px;box-shadow:0 4px 4px rgba(0,0,0,.08),0 4px 8px rgba(0,0,0,.12),0 4px 16px rgba(0,0,0,.24);cursor:pointer;align-self:center;">Redefinir senha</a>');
+        $mail->setContent('<a href="http://localhost/captiveportal/views/resetPassword.php?email='.$EncodeEmail.'"><button>Redefinir senha</button></a>');
         $mail->setContent('</div>');
         $mail->setContent('<div style="position:relative;bottom:10px;text-align:center;font-size:15px;">');
         $mail->setContent('<b style="font-size:20px;">Enviado por Unimed São José dos Campos - Cooperativa de Trabalho Médico &copy; 2023</b>');
@@ -96,11 +92,10 @@ class mailController{
         // CONEXAO EMAIL
         $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
-        $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-        $phpmailer->SMTPAuth = true;
-        $phpmailer->Port = 2525;
-        $phpmailer->Username = '4075d522bd2a32';
-        $phpmailer->Password = 'b014bec44d1bc8';
+        $phpmailer->Host = 'mail.unimedsjc.com.br';
+        $phpmailer->Port = 25;
+        $phpmailer->SMTPAuth = false;
+        $phpmailer->SMTPAutoTLS = false;
         $phpmailer->CharSet = "UTF-8";
 
         // CONEXAO BANCO
@@ -139,7 +134,7 @@ class mailController{
         $mail->setContent('<h2>Olá :]</h2>');
         $mail->setContent('<p>Ficamos sabendo que você quer se descadastrar☹️</p>');
         $mail->setContent('<p style="margin-bottom: 40px; font-weight:bold; color:red;">Caso não tenho sido você a pedir a redefinição de senha, pode ignorar esse e-mail. Caso tenha sido, clique no link abaixo.😉</p>');
-        $mail->setContent('<a href="http://localhost/captiveportal/views/unsubscribeAccept.php?email='.$EncodeEmail.'" style="text-align:center;font-size:25px;text-decoration:none;background-color:rgb(67,119,67);color:white;padding:20px 26px;border:2px solid rgb(67,119,67);border-radius:40px;box-shadow:0 4px 4px rgba(0,0,0,.08),0 4px 8px rgba(0,0,0,.12),0 4px 16px rgba(0,0,0,.24);cursor:pointer;align-self:center;">Descadastrar</a>');
+        $mail->setContent('<a href="http://localhost/captiveportal/views/unsubscribeAccept.php?email='.$EncodeEmail.'"><button>Descadastrar</button></a>');
         $mail->setContent('</div>');
         $mail->setContent('<div style="position:relative;bottom:10px;text-align:center;font-size:15px;">');
         $mail->setContent('<b style="font-size:20px;">Enviado por Unimed São José dos Campos - Cooperativa de Trabalho Médico &copy; 2023</b>');
