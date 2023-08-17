c37d89c3c5717011d4c2d35ff292c5411a668a0c
Allan Mauricio <allan.work19@gmail.com>
2023-08-17 10:33:55 -0300

    Adicao do env

--- Model/conexao.php
+++ Model/conexao.php
@@ -3,8 +3,8 @@
         private static $instance;
         public static function getConexao(){
             $env = new env();
-            $user = $env->GetDbUser();
-            $pass = $env->GetDbPassword();
+            $user = $env->getDbUser();
+            $pass = $env->getDbPassword();
 
             if(!isset(self::$instance)){
                 self::$instance = new \PDO('mysql:host=localhost;dbname=hotspot;charset=utf8', $user, $pass);
